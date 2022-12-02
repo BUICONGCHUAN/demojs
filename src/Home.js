419174d6dd5e7e786938d4d99f8ad4cdc81aa054
Bùi Công Chuẩn <97036847+BUICONGCHUAN@users.noreply.github.com>
2022-12-02 19:22:50 +0700

    demo

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+export default function Home() {
+  return (
+    <div className='yeuPA'>
+          <div className='lovePA'>yêu phương anh</div>
+          <img src="https://tinhhoacongnghe.com/wp-content/uploads/2022/09/cach-tao-anh-chu-xep-thanh-hinh-trai-tim-anh-3.jpg" alt=""/>
+
+    </div>
+    
+  ) 
+}
